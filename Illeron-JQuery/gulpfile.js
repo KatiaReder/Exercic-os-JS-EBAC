833f4f56e4ec25e4358f896bebd7987cf271dc58
Katia <katireder5@gmail.com>
2023-04-03 11:53:58 -0300

    Alterações

--- Illeron-JQuery/gulpfile.js
+++ Illeron-JQuery/gulpfile.js
@@ -1,31 +1,72 @@
 const gulp = require('gulp')
+const { parallel, series} = require('gulp')
 const concat = require('gulp-concat')
 const cssmin = require('gulp-cssmin')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
+const htmlmin = require('gulp-htmlmin')
+const babel = require('gulp-babel')
+const browserSync = require('browser-sync').create()
+const reload = browserSync.reload
 
-function tarefasCSS(cb) {
+function tarefasCSS(callback) {
 
-   return gulp.src(['./css/style.css','./node_modules/bootstrap/dist/css/bootstrap.css',
+   gulp.src(['./src/css/style.css','./node_modules/bootstrap/dist/css/bootstrap.css',
     './vendor/owl/css/owl.css', './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css', './vendor/jquery-ui/jquery-ui.css'])
         .pipe(concat('styles.css'))
         .pipe(cssmin())
         .pipe(rename({ suffix: '.min'})) 
         .pipe(gulp.dest('./dist/css'))
 
+       return callback()
+
 }
 
-function tarefasJS(){
+function tarefasJS(callback){
 
-    return gulp.src(['./js/custom.js','./node_modules/jquery/dist/jquery.js', 
+    gulp.src(['./src/js/custom.js','./node_modules/jquery/dist/jquery.js', 
     './node_modules/bootstrap/dist/js/bootstrap.js', './vendor/owl/js/owl.js', 
-    './vendor/jquery-mask/jquery.mask.js', './vendor/jquery-ui/jquery-ui.js'])
+    './vendor/jquery-mask/jquery.mask.js'])
+        .pipe(babel({
+            comments: false,
+            presets: ['@babel/env']
+        }))
         .pipe(concat('scripts.js'))
         .pipe(uglify())
         .pipe(rename({ suffix: '.min'}))
         .pipe(gulp.dest('./dist/js'))
+
+       return callback()
 }
 
+function tarefasHTML(callback) {
+    gulp.src('./src/**/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('./dist'))
 
+    return callback()
+}
+
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    })
+    gulp.watch('./src/**/*').on('change', process) 
+    gulp.watch('./src/**/*').on('change', reload)
+})
+
+function end(callback) {
+    console.log("Tarefas Concluídas")
+    return callback
+}
+
+const process =  parallel(tarefasHTML, tarefasCSS, tarefasJS, end) 
 exports.styles = tarefasCSS
 exports.scripts = tarefasJS
+
+exports.default = process
+
+
+exports.default = parallel(tarefasHTML, tarefasCSS, tarefasJS)
--- gulp/projeto/gulpfile.js
+++ gulp/projeto/gulpfile.js
@@ -1,14 +1,19 @@
-const gulp1 = import('gulp')
+
 const gulp = require('gulp')
+const { parallel, series} = require('gulp')
 
 //ambas foram importadas pelo npm
 const concat = require('gulp-concat')
 const cssmin = require('gulp-cssmin')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
-const imagemin = import('gulp-imagemin')
+const htmlmin = require('gulp-htmlmin')
+const babel = require('gulp-babel')
+const browserSync = require('browser-sync').create()
+const reload = browserSync.reload
 
-function tarefasCSS(cb) {
+
+function tarefasCSS(callback) {
     //não vou usar mais a pasta vendor no projeto
     //vou minificar esses arquivos
     //concat() -> junta os arquivos
@@ -17,7 +22,7 @@ function tarefasCSS(cb) {
     //gulp.dest() onde os arquivos vão ser colocados depois de processado
     // gulp.src() vai achar qualquer arquivo com .css
     //suffix vai obrigar a adição de min no nome
-   return gulp.src(['./node_modules/bootstrap/dist/css/bootstrap.css',
+    gulp.src(['./node_modules/bootstrap/dist/css/bootstrap.css',
     './vendor/owl/css/owl.css', 
     'node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
 './vendor/owl/css/owl.css'])
@@ -25,48 +30,77 @@ function tarefasCSS(cb) {
    .pipe(cssmin())
    .pipe(rename({suffix: '.min'})) //libs.min.css
    .pipe(gulp.dest('./dist/css'))
+
+   return callback()
 }
 
-function tarefasJS() {
-    return gulp.src('./vendor/**/*.js')
+function tarefasJS(callback) {
+     gulp.src('./vendor/**/*.js')
+     .pipe(babel({
+        comments: false,
+        presets: ['@babel/env']
+     }))
     .pipe(concat('libs.js'))
     .pipe(uglify())
     .pipe(rename({suffix: '.min'})) //libs.min.js
     .pipe(gulp.dest('./dist/js'))
+
+    return callback()
 }
 
 // function tarefasImagem() {
-//     return gulp.src('./src/images/*')
-//     .pipe(imagemin([
-// 	imagemin.gifsicle({interlaced: true}),
-// 	imagemin.mozjpeg({quality: 75, progressive: true}),
-// 	imagemin.optipng({optimizationLevel: 5}),
-// 	imagemin.svgo({
-// 		plugins: [
-// 			{removeViewBox: true},
-// 			{cleanupIDs: false}
-// 		]
-// 	})
-// ]))
-//     .pipe(gulp.dest('./dist/images'))
+// 	return gulp.src('./src/images/*')
+// 	     .pipe(imagemin({
+//             pngquant: true,
+//             optipng: false,
+//             zopflipng: true,
+//             jpegRecompress: false,
+//             mozjpeg: true,
+//             gifsicle: true,
+//             svgo: true,
+//             concurrent: 10,
+//             quiet: true
+//         }))
+// 		.pipe(gulp.dest('./dist/images'))
 // }
 
-function tarefasImagem() {
-	return gulp.src('./src/images/*')
-	     .pipe(imagemin({
-            pngquant: true,
-            optipng: false,
-            zopflipng: true,
-            jpegRecompress: false,
-            mozjpeg: true,
-            gifsicle: true,
-            svgo: true,
-            concurrent: 10,
-            quiet: true
-        }))
-		.pipe(gulp.dest('./dist/images'))
+
+//trabalhar com html
+
+function tarefasHTML(callback) {
+    gulp.src('./src/**/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('./dist'))
+
+    return callback()
+    //qualquer alteração que deva ser feita, tem que fazer no html padrão, não no que está no dist
 }
 
-exports.styles = tarefasCSS
-exports.scripts = tarefasJS
-exports.images = tarefasImagem
+//cria um novo server pelo gulp
+// comando para rodar -> "gulp serve"
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    })
+     gulp.watch('./src/**/*').on('change', process) //repete o processo quando alterar algo em src
+    gulp.watch('./src/**/*').on('change', reload)
+})
+
+function end(callback) {
+    console.log("tarefas concluídas")
+    return callback
+}
+
+
+const process =  series(tarefasHTML, tarefasCSS, tarefasJS, end) //
+
+exports.default = process
+//para rodar assim tem que ter o return de callback e no terminar usa apenas "gulp"
+// exports.styles = tarefasCSS
+// exports.scripts = tarefasJS
+// exports.images = tarefasImagem
+
+
+//rodar servidor do node no proj que tá no dist - $ http-server ./dist
